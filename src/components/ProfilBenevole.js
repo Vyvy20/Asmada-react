37aa88bd50891116324b9c130fa6accb57fa628b
Vyvy20 <c.dang2001@gmail.com>
2022-05-29 14:34:34 +0200

    Commentaires ajoutés

--- src/components/ProfilBenevole.js
+++ src/components/ProfilBenevole.js
@@ -4,14 +4,17 @@ import "../Css/ProfilBenevole.css"
 function ProfilB() {
   return (
     <div classname='ProfilGlobal'>
+      {/* Bloc des titres */}
       <div className='TitreProfil'>
         <h4 className='NomBenevole'> Bénévole {'>'} Camille DANG</h4>
         <hr className='SepProfil'></hr>
       </div>
 
+      {/* Contenu du profil contenant le menu, les informations du bénévoles et un bloc pour candidater */}
       <div className='ContenuProfil'>
         <h3 className='MsgBienvenue'>Bienvenue Camille</h3>
 
+        {/* Bloc du menu */}
         <div className='MenuB'>
           <input type='submit' className='btnCompte' value='Mon Compte'/> <br className='EspaceC'></br>
           <input type='submit' className='btnProfil' value='Mon Profil'/> <br className='EspaceP'></br>
@@ -20,6 +23,7 @@ function ProfilB() {
           <input type='submit' className='btnDeco' value='Déconnexion'/> <br className='EspaceD'></br>
         </div>
 
+        {/* Bloc des informations du bénévoles */}
         <div className='InfoBenevole'>
           <h5 className='TitreCoord'>Mes coordonnées</h5>
           <label className='Prenom'>Prénom : Camille </label><br></br>
@@ -35,6 +39,7 @@ function ProfilB() {
           <a className='ContactConseiller' href='*'>Contacter un conseiller</a><br></br>
           <input type='submit' className='BtnModifInfos' value='Je modifie mes informations'/><br></br>
           
+          {/* Bloc pour candidater aux missions */}
           <div className='ConsultMissions'>
             <h5 className='TitreConsultMission'>Consultez les missions bénévoles proposées par l'association</h5>
             <input type='submit' className='BtnRenseigne' value='Je renseigne mon profil pour trouver des&#13;&#10;missions qui me correspondent'/>
