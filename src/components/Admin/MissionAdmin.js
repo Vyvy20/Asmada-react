87d2335fc23138dcf0f5c668205c2d6c15939df5
BOUGHANIME_Ibra <boughanimeibra@gmail.com>
2022-06-13 15:52:23 +0200

    résolution des problèmes dans code Camille

---
+++ src/components/Admin/MissionAdmin.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import "../../Css/MissionAdmin.css"
+
+function MissionAdmin() {
+    return (
+
+        <div className='MissionsAssoAdmin'>
+
+            <h1 className='TitrePageMissA'>Missions proposées</h1>
+            <hr className='SepMissA'></hr>
+            <input type='submit' className='btnAjouterMiss' value='Ajouter une mission'></input>
+            
+            {/* Bloc des missions */}
+            <div className='LstMissionsAdmin'>
+
+                {/* Bloc première mission */}
+                <div className='MissionNum1A'>
+
+                    <h2 className='TitreMission1A'>Professeur de soutien en programmation</h2>
+                    <label className='DureeA'>Durée : </label>
+                    <p>3 semaines</p>
+                    <label className='LieuA'>Lieu : </label>
+                    <p>Salary</p>
+                    <label className='ThèmeA'>Thèmes : </label>
+                    <p>Programmation, Education</p>
+                    <input className='btnModif' type="submit" value="Modifier " />
+                    <input className='btnSupp' type="submit" value="Supprimer " />
+
+                </div>
+
+                {/* Bloc deuxième mission */}
+                <div className='MissionNum2A'>
+
+                    <h2 className='TitreMission1A'>Electricien</h2>
+                    <label className='DureeA'>Durée : </label>
+                    <p>2 semaines</p>
+                    <label className='LieuA'>Lieu : </label>
+                    <p>Ankara</p>
+                    <label className='ThèmeA'>Thèmes : </label>
+                    <p>Electricité</p>
+                    <input className='btnModif' type="submit" value="Modifier " />
+                    <input className='btnSupp' type="submit" value="Supprimer " />
+                    
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default MissionAdmin;
---
+++ src/components/Benevole/Mission.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import "../../Css/Mission.css"
+
+function Mission() {
+    return (
+
+        <div className='MissionsAsso'>
+
+            <h1 className='TitrePageMiss'>Missions proposées</h1>
+            <hr className='SepMiss'></hr>
+
+            {/* Bloc des missions */}
+            <div className='LstMissions'>
+
+                {/*
+                <form className='FrmFiltre'>
+                    <div className='Filtres'>
+                        <h2 className='TitreFiltresRech'>Filtres de recherche</h2>
+                        <label className='LblMotCle'>Mot-clé :</label>
+                        <input type='text' className='TxtMotCle' value='Entrer un mot clé'></input>
+                        <label className='LblThm'>Thème : </label>
+                        <input type='text' className='TxtThm' value='Entrer un thème'></input> <br></br>
+                        <input type='submit' className='BtnRech' value='Rechercher'></input>
+                    </div>
+                </form>*/}
+
+                {/* Bloc première mission */}
+                <div className='MissionNum1'>
+
+                    <a className='TitreMission1' href='*'>Professeur de soutien en programmation</a><br></br>
+                    <label className='Duree'>Durée : </label>
+                    <p>3 semaines</p>
+                    <label className='Lieu'>Lieu : </label>
+                    <p>Salary</p>
+                    <label className='Thème'>Thèmes : </label>
+                    <p>Programmation, Education</p>
+
+                </div>
+
+                {/* Bloc deuxième mission */}
+                <div className='MissionNum2'>
+
+                    <a className='TitreMission1' href='*'>Electricien</a><br></br>
+                    <label className='Duree'>Durée : </label>
+                    <p>2 semaines</p>
+                    <label className='Lieu'>Lieu : </label>
+                    <p>Ankara</p>
+                    <label className='Thème'>Thèmes : </label>
+                    <p>Electricité</p>
+                    
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Mission;
