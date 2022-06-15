9bc496a71f46f691e32f9e411e8afa1907ded304
Vyvy20 <c.dang2001@gmail.com>
2022-06-15 23:15:33 +0200

    Amélioration page actualités

--- src/components/Actu.js
+++ src/components/Actu.js
@@ -1,48 +1,95 @@
 import React, {Component} from 'react';
 import "../Css/Actu.css"
+const PictureAsmada = new URL("../images/Asmada.jpg", import.meta.url)
+const PictureNews = new URL("../images/Journal.jpeg", import.meta.url)
+const PictureEcole = new URL("../images/ecole.jpg", import.meta.url)
+const PictureHop= new URL("../images/Hôpital.jpg", import.meta.url)
+const PicturePot= new URL("../images/potager.jpg", import.meta.url)
+const PictureBien= new URL("../images/Bienvenue.png", import.meta.url)
+const PictureChanti= new URL("../images/Chantier.jpg", import.meta.url)
+const PictureSol= new URL("../images/solaire.jpg", import.meta.url)
 
 function Actu() {
     return (
 
-        <div className='Actu'>
+        <div className="page-top">
 
-            <h1 className='TitrePageActu'>Actualités du moment</h1>
-            <hr className='SepActu'></hr>
+            <div> 
+                <img className="Photo1Miss" src={PictureNews} />
+            </div>
+            <div className="logoAsmada"> 
+                <img className="Photo2" src={PictureAsmada} />
+            </div><br/><br/>
+
+             <section className="resume-Titre">
+                 
+                <div className="resume-section-content">
+                    <h1 className="Titre">
+                    Actualités du moment
+                    </h1>
+                </div><br/><br/>
+            </section>
 
             {/* Bloc des actualités */}
             <div className='LstActu'>
 
-                {/*
-                <form className='FrmFiltre'>
-
-                    <div className='Filtres'>
-
-                        <h2 className='TitreFiltresRech'>Filtres de recherche</h2>
-                        <label className='LblMotCle'>Mot-clé :</label>
-                        <input type='text' className='TxtMotCle' value='Entrer un mot clé'></input>
-                        <label className='LblThm'>Thème : </label>
-                        <input type='text' className='TxtThm' value='Entrer un thème'></input> <br></br>
-                        <input type='submit' className='BtnRech' value='Rechercher'></input>
-
-                    </div>
-
-                </form>*/}
-
                 {/* Bloc première actualité */}
                 <div className='ActuNum1'>
 
-                    <a className='TitreActu1' href='*'>Pronuntiante puniri sorte cum sorte</a><br></br>
+                    <a className='TitreActu1' href='*'>Une école est ouverte !</a><br></br>
+                    <img className="Photo2Actu" src={PictureEcole} /><br></br>
                     <label className='LblResumeNum1'>Résumé</label>
                     <p className='ResumeActu1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
                 </div>
 
-                {/* Bloc deuxième mission */}
+                {/* Bloc deuxième actualité */}
                 <div className='ActuNum2'>
 
-                    <a className='TitreActu1' href='*'>Test du moment</a><br></br>
-                    <label className='LblResumeNum2'>Résumé</label>
-                    <p className='ResumeActu2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <a className='TitreActu1'>Construction d'un hôpital</a><br></br>
+                    <img className="Photo2Actu" src={PictureHop} /><br></br>
+                    <label className='LblResumeNum1'>Résumé</label>
+                    <p className='ResumeActu1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    
+                </div>
+
+                {/* Bloc troisième actualité */}
+                <div className='ActuNum3'>
+
+                    <a className='TitreActu1'>Projet d'un potager</a><br></br>
+                    <img className="Photo2Actu" src={PicturePot} /><br></br>
+                    <label className='LblResumeNum1'>Résumé</label>
+                    <p className='ResumeActu1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    
+                </div>
+
+                 {/* Bloc quatrième actualité */}
+                 <div className='ActuNum4'>
+
+                    <a className='TitreActu1'>Des idées de chantier</a><br></br>
+                    <img className="Photo2Actu" src={PictureChanti} /><br></br>
+                    <label className='LblResumeNum1'>Résumé</label>
+                    <p className='ResumeActu1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                </div>
+
+                {/* Bloc cinquième actualité */}
+                <div className='ActuNum5'>
+
+                    <a className='TitreActu1'>Panneaux solaires pour le lycée</a><br></br>
+                    <img className="Photo2Actu" src={PictureSol} /><br></br>
+                    <label className='LblResumeNum1'>Résumé</label>
+                    <p className='ResumeActu1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                </div>
+
+                {/* Bloc sixième actualité */}
+                <div className='ActuNum6'>
+
+                    <a className='TitreActu1'>Bienvenue aux nouveaux membres !</a><br></br>
+                    <img className="Photo2Actu" src={PictureBien} /><br></br>
+                    <label className='LblResumeNum1'>Résumé</label>
+                    <p className='ResumeActu1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     
                 </div>
 
--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ function App(){
       <div>
          <Header />
          <Nav/>
-         <DetailMission/>
+         <Actu/>
          <Footer />
       </div>
    );
